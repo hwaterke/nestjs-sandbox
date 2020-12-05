b59b7d4042b239f09d45770b2b85e42c690a86dd
hwaterke <hwaterke@users.noreply.github.com>
2020-12-05 17:58:49 +0100

    Adds typeorm configuration for dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import {Module} from '@nestjs/common'
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {databaseConfig} from './config/database.config'
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseConfig)],
   providers: [],
 })
 export class AppModule {}
