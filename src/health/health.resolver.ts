c0a812d6578539206ae242541bd5ba4e91319c43
hwaterke <hwaterke@users.noreply.github.com>
2020-12-13 15:54:38 +0100

    Adds node env to the health check response

--- src/health/health.resolver.ts
+++ src/health/health.resolver.ts
@@ -1,9 +1,13 @@
 import {Query, Resolver} from '@nestjs/graphql'
+import {HealthObjectType} from './types/health.type'
+import {nodeEnv} from '../config/env'
 
 @Resolver()
 export class HealthResolver {
-  @Query(() => String)
-  health(): string {
-    return 'ok'
+  @Query(() => HealthObjectType)
+  health(): HealthObjectType {
+    return {
+      nodeEnv: nodeEnv(),
+    }
   }
 }
