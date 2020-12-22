b0b3a38ef48964df1d390e83211c1a707cfcaa59
hwaterke <hwaterke@users.noreply.github.com>
2020-12-22 17:26:09 +0100

    Prevents registration of a user with an email already used

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,12 +11,22 @@ export class AuthService {
     @InjectRepository(User) private userRepository: Repository<User>
   ) {}
 
-  register(registerInput: RegisterInput): Promise<UserObjectType> {
+  async register(registerInput: RegisterInput): Promise<UserObjectType> {
+    const emailAvailable = await this.isEmailAvailable(registerInput.email)
+    if (!emailAvailable) {
+      throw new Error('EMAIL_ALREADY_USED')
+    }
+
     const user = this.userRepository.create({
       email: registerInput.email,
       password: registerInput.password,
     })
 
     return this.userRepository.save(user)
+  }
+
+  async isEmailAvailable(email: string) {
+    const totalEmails = await this.userRepository.count({email})
+    return totalEmails === 0
   }
 }
