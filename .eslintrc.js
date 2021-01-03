ae98615ff83b4163bf827e67a3335957008373e4
hwaterke <hwaterke@users.noreply.github.com>
2021-01-03 11:36:28 +0100

    Enforces @Entity filenames with custom eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'custom'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -28,5 +28,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'custom/typeorm-entity-filename': 'error',
   },
 }
