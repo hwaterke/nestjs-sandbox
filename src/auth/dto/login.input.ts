051b4e6270b4af8ad6c888b54944d045ce46c856
hwaterke <hwaterke@users.noreply.github.com>
2021-12-18 17:40:46 +0100

    Improves auth DTOs

--- src/auth/dto/login.input.ts
+++ src/auth/dto/login.input.ts
@@ -1,8 +1,10 @@
 import {ArgsType, Field} from '@nestjs/graphql'
+import {IsEmail} from 'class-validator'
 
 @ArgsType()
 export class LoginInput {
   @Field()
+  @IsEmail()
   email!: string
 
   @Field()
--- src/auth/dto/register.input.ts
+++ src/auth/dto/register.input.ts
@@ -1,13 +1,14 @@
 import {ArgsType, Field} from '@nestjs/graphql'
-import {IsEmail, MinLength} from 'class-validator'
+import {IsEmail, Length} from 'class-validator'
+import {User} from '../../users/entities/user.entity'
 
 @ArgsType()
-export class RegisterInput {
-  @IsEmail()
+export class RegisterInput implements Partial<User> {
   @Field()
+  @IsEmail()
   email!: string
 
-  @MinLength(8)
   @Field()
+  @Length(8, 250)
   password!: string
 }
